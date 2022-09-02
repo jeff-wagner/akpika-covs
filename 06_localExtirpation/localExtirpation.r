2e906141e8e93430a7cd14cc3db3cac5d23417b1
jeff-wagner <60358063+jeff-wagner@users.noreply.github.com>
2022-09-02 13:38:31 -0600

    Updated model script

--- 06_localExtirpation/localExtirpation.r
+++ 06_localExtirpation/localExtirpation.r
@@ -90,7 +90,7 @@ nregions <- nlevels(siteOcc$region)
 mod <- "
 model{
   for(i in 1:length(y)){
-        y[i] ~ dbinom(p[i], 1)  # Each observation follows a binomial distribution with 1 trial and probability of being occupied (p)
+        y[i] ~ dbinom(p[i], 1)  # Each observation follows a binomial distribution with 1 trial and probability of extirpation (p)
         logit(p[i]) = b0[r[i]] + b1[r[i]]*x1[i] + b2[r[i]]*x2[i] + b3[r[i]]*x3[i]  # this probability has an inverse logit relationship with the 
   }
     for(r in 1:nregions){
@@ -110,29 +110,90 @@ model{
     tau3 ~ dgamma(0.001, 0.001)
 }
 "
-
-dataList <- list(r=siteOcc$region.num,
-                 nregions=nregions,
+dataList <- list(region=siteOcc$region.num,
                  y=siteOcc$occ.status,
                  x1=siteOcc$st.precip,
                  x2=siteOcc$st.summerWarmth,
                  x3=siteOcc$st.januaryMinTemp)
-params <- c("b0", "b1", "b2", "b3")
+
+reprex({
+  library(rjags)
+  library(jagsUI)
+  
+  dataList <- list(region= c(1,1,1,1,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,1,1,2,2,2,2,1,1,2,2,1,1),
+                   y= c(0,0,0,0,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0),
+                   x1= runif(31,-1,1),
+                   x2= runif(31,-1,1),
+                   x3= runif(31,-1,1))
+  
+  mod <- "
+model{
+  for(i in 1:length(y)){
+        y[i] ~ dbern(p[region[i]]) 
+  }
+  for(r in 1:max(region)){
+          logit(p[region[i]] = b0[r] + b1[r]*x1[r[i]] + b2[r]*x2[r[i]] + b3[r]*x3[r[i]]
+          b0[r] ~ dnorm(sigma, tau)
+          b1[r] ~ dnorm(sigma, tau)
+          b2[r] ~ dnorm(sigma, tau)
+          b3[r] ~ dnorm(sigma, tau)
+  }
+    
+    sigma ~ dnorm(0, 0.0001)
+    tau ~ dgamma(0.001, 0.001)
+
+}
+"
+})
+
+params <- c("b0", "b1", "b2", "b3", "p")
+
+
+# Bayesian model, this is JAGS code
+mod1 <- "
+model{
+  for(i in 1:length(y)){
+        y[i] ~ dbern(p[region[i]]) 
+  }
+  for(r in 1:max(region)){
+          # logit(p[r[i]]) = b0[r] + b1[r]*x1[r[i]] + b2[r]*x2[r[i]] + b3[r]*x3[r[i]]
+          logit(p[r[i]]) = b0 + b1*x1[r[i]] + b2*x2[r[i]] + b3*x3[r[i]]
+          # b0[r] ~ dnorm(sigma, tau)
+          # b1[r] ~ dnorm(sigma, tau)
+          # b2[r] ~ dnorm(sigma, tau)
+          # b3[r] ~ dnorm(sigma, tau)
+  }
+    b0 ~ dnorm(0, 0.0001)
+    b1 ~ dnorm(0, 0.0001)
+    b2 ~ dnorm(0, 0.0001)
+    b3 ~ dnorm(0, 0.0001)
+    # sigma ~ dnorm(0, 0.0001)
+    # tau ~ dgamma(0.001, 0.001)
+
+}
+"
+
+dataList1 <- list(region=siteOcc$region.num,
+                 y=siteOcc$occ.status,
+                 x1=siteOcc$precip,
+                 x2=siteOcc$summerWarmth,
+                 x3=siteOcc$januaryMinTemp)
+params <- c("b0", "b1", "b2", "b3", "p")
 inits <- NULL
 
-output <- jags(data = dataList,
+output <- jags(data = dataList1,
                inits = inits,
                parameters.to.save = params,
-               model.file = textConnection(mod),
+               model.file = textConnection(mod1),
                n.chains = 3,
                n.adapt = 10000,
-               n.iter = 1001000,
-               n.burnin = 100000,
-               n.thin = 10,
+               n.iter = 1000000,
+               n.burnin = 1000,
+               n.thin = 100,
                parallel = FALSE,
                DIC = TRUE)
 output
 
 ## diagnostic plots
-traceplot(output, parameters=c("b0", "b1", "b2", "b3"))
-autocorr.plot(output$samples[,1:3],lag.max=40,auto.layout=TRUE,ask=TRUE)
+traceplot(output, parameters=c("b0", "b1", "b2", "b3", "p"))
+autocorr.plot(output$samples[,1:7],lag.max=40,auto.layout=TRUE,ask=TRUE)
